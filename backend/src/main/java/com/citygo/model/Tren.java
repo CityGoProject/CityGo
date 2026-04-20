724cf2d6d8799e308ba3ddf9fa1a092a3fd317ea
WertC-14 <mustafamertcevik@gmail.com>
2026-04-20 10:13:39 +0300

    feat: Tren alt sınıfı eklendi

--- backend/src/main/java/com/citygo/model/Tren.java
+++ backend/src/main/java/com/citygo/model/Tren.java
@@ -1,4 +1,5 @@
 package com.citygo.model;
+import jakarta.persistence.*;
 
 /*
  * =============================================================
@@ -32,3 +33,41 @@ package com.citygo.model;
  * Getter/Setter:
  * - Ek alanlar için getter/setter metotları
  */
+
+@Entity
+@DiscriminatorValue("TREN")
+public class Tren extends UlasimAraci{
+
+    private String vagonTipi;
+    private String hatTipi;
+
+    public String getVagonTipi() {return vagonTipi;}
+    public void setVagonTipi(String vagonTipi) {this.vagonTipi = vagonTipi;}
+
+    public String getHatTipi() {return hatTipi;}
+    public void setHatTipi(String hatTipi) {this.hatTipi = hatTipi}
+
+    @Override 
+    public String getAracTipi(){return "TREN";}
+
+    @Override
+    public double hesaplaToplamFiyat(double temelFiyat) {
+        if ( vagonTipi == "BUSINESS" and hatTipi == "YHT") 
+            { 
+                return (temelFiyat * 1.5)*1.2;
+            }
+
+        else if (vagonTipi == "EKONOMI" and hatTipi == "YHT")
+            {
+                return (temelFiyat)*1.2;
+            }
+
+        else if (vagonTipi == "BUSINESS" and hatTipi == "NORMAL")
+            {
+                return (temelFiyat)*1.5;
+            }
+
+        else {return temelFiyat;}
+    }
+
+}
