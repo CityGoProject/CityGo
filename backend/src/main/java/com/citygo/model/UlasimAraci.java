a4831ff0eedd45d41ab06c88e1735b35bd4e60c7
WertC-14 <mustafamertcevik@gmail.com>
2026-04-20 08:55:44 +0300

    feat: UlasimAraci abstract sınıfı eklendi

--- backend/src/main/java/com/citygo/model/UlasimAraci.java
+++ backend/src/main/java/com/citygo/model/UlasimAraci.java
@@ -1,4 +1,55 @@
 package com.citygo.model;
+import jakarta.persistence.*;
+
+@Entity // Veritabanını tablo ile eşleştirme
+@Table(name="ulasimaraci") // Tablo ismini ayarlıyorum 
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // Tüm alt sınıfları tek tablo ile saklar
+@DiscriminatorColumn(name="arac_tipi") // Tabloda arac_tipi sutunu ile ayırt eder.
+public abstract class  UlasimAraci {
+
+    // Otomatik artan id 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Kolon isimleri atama (name) ve her araçta yazılması zorun hale getirme (nullable = false)
+    @Column(name="firma",nullable = false)
+    private String firma;
+    
+    @Column(name="model",nullable = false)
+    private String model;       
+    
+    @Column(name="kapasite",nullable = false)
+    private int kapasite;
+    
+    @Column(name="bilet_fiyati",nullable = false)
+    private double biletFiyati;
+    
+    //getter/setter'lar
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    
+    public String getFirma () {return firma;}
+    public void setFirma(String firma) {this.firma = firma;}
+    
+    public String getModel() {return model;}
+    public void setModel(String model) {this.model = model;}
+    
+    public int getKapasite() {return kapasite;}
+    public void setKapasite(int kapasite) {this.kapasite = kapasite;}
+
+    public double getBiletFiyati() {return biletFiyati;}
+    public void setBiletFiyati(double biletFiyati) {this.biletFiyati = biletFiyati;}
+
+    // Alt sınıflara kendi fiyatlarını hesaplama implement etme
+    public abstract double hesaplaToplamFiyat(double temelFiyat);
+    // Alt sınıflar kendi araç tipini döndürür ("UCAK","TREN","OTOBUS")
+    public abstract String getAracTipi();
+    
+} 
+
+
+
 
 /*
  * =============================================================
